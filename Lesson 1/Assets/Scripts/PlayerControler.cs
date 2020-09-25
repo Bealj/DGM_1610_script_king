2fa8f4dc5976a36bab5da5b4341bcf96e7dab536
Joram Beal <10923637@my.uvu.edu>
2020-09-25 16:54:38 -0600

    Added assets to lesson 1

---
+++ Lesson 1/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public int speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move vehichle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
