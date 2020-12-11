3c0efe2fe173e85d84311a2df28b3f31395059da
Joram Beal <10923637@my.uvu.edu>
2020-12-10 17:42:30 -0700

    Added lesson 2 and prototype 1

--- Lesson 2 - Feed the Animals/Assets/Scripts/MoveForward.cs
+++ Lesson 2 - Feed the Animals/Assets/Scripts/MoveForward.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class MoveForward : MonoBehaviour
 {
-    public float horizontalInput;
-
+    public float speed = 40f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input. GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
