a32ee2378a65c74f5bc6cc07b160767a5cde1cf5
Joram Beal <10923637@my.uvu.edu>
2020-12-12 14:55:24 -0700

    Finished prototype1

--- Prototype1/Assets/Scripts/PlayerContoller.cs
+++ Prototype1/Assets/Scripts/PlayerContoller.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,11 @@ public class PlayerContoller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We'll turn the vechicle
+         transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
